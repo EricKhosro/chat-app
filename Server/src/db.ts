0c8408eff947329ce18b7bdd5e7fe36ffdbb70ab
Eric Khosrafian <erickhosrafian@gmail.com>
2023-04-05 18:36:13 +0430

    added id to IUser

--- Server/src/db.ts
+++ Server/src/db.ts
@@ -1,6 +1,6 @@
-import { ILoginData } from "./interfaces/interfaces";
+import { IUser } from "./interfaces/interfaces";
 
-export const users: Array<ILoginData> = [
-  { username: "user1", password: "123"},
-  { username: "user2", password: "321"},
+export const users: Array<IUser> = [
+  { username: "user1", password: "123", id: 1 },
+  { username: "user2", password: "321", id: 2 },
 ];
