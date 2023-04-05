652b30faff68fb2b469425005419d1c717d1e597
Eric Khosrafian <erickhosrafian@gmail.com>
2023-04-05 18:36:51 +0430

    added Methods folder

--- Server/src/methodFactory.ts
+++ Server/src/methodFactory.ts
@@ -1,5 +1,5 @@
 import { IMethodClass, IMethodFactory } from "./interfaces/interfaces";
-import { Login } from "./login.js";
+import { Login } from "./Methods/login";
 
 export default class MethodFactory implements IMethodFactory<any> {
   #methodName: string;
