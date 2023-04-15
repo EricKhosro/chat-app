bb4a4104f8ece6462d41c3073c3d798474854700
Eric Khosro <ericzed2002@gmail.com>
2023-04-15 15:18:48 +0330

    added ISocket IServer ISocketEvents

--- Server/src/interfaces/interfaces.ts
+++ Server/src/interfaces/interfaces.ts
@@ -1,3 +1,5 @@
+import { Socket } from "net";
+
 export interface IPacket<T> {
   msg: string;
   errorCode?: number;
@@ -18,8 +20,21 @@ export interface IRequestData<T> {
   body: T;
 }
 
-export interface IConnection<T> {
-  createConnection: () => T;
+export interface IServer {
+  createServer: () => void;
+  setOnNewSocket: (onNewSocket: (socket: ISocket) => void) => void;
+  startListening: (port: number, host: string) => void;
+}
+
+export interface ISocketEvents {
+  onData: (data: Buffer) => void;
+  onDisconnect: () => void;
+  onError: (error: Error) => void;
+}
+
+export interface ISocket {
+  setEvents: (events: ISocketEvents) => void;
+  sendData: (data: Buffer) => void;
 }
 
 export interface IFriends {
@@ -31,4 +46,13 @@ export interface IUser {
   username: string;
   password: string;
   id: number;
+}
+
+export interface ISocketHandler {
+  registerSocker: (socket: ISocket) => void;
+}
+
+export interface IRegisteredSocket {
+  socket: ISocket;
+  guid: string;
 }
