6483fe31ebdb14e3ed4bd5762631f79879e2fc51
Eric Khosro <ericzed2002@gmail.com>
2023-04-15 15:16:24 +0330

    added getUserByID

--- Server/src/db.ts
+++ Server/src/db.ts
@@ -1,21 +1,35 @@
+import { IGetUsersData } from "./interfaces/getUsersInterface";
 import { IUser } from "./interfaces/interfaces";
 
-export const users: Array<IUser> = [
-  { username: "user1", password: "123", id: 1 },
-  { username: "user2", password: "321", id: 2 },
-];
-export class DataBase {
+export class Database {
   #users: Array<IUser> = [
     { username: "user1", password: "123", id: 1 },
     { username: "user2", password: "321", id: 2 },
   ];
 
-  public getUsers = () => this.#users;
-
-  public setUser = (user: IUser) => {
-    const targetUser = users.find((u) => {
-      return u.id === user.id;
+  public getUsersByID = (userIds?: Array<IGetUsersData>) => {
+    if (!userIds) return this.#users;
+    const res: Array<IUser> = [];
+    userIds.forEach((uId) => {
+      
+      const targetUser = this.#users.find((u) => {
+        return u.id === uId.id;
+      });
+      if (targetUser) res.push(targetUser);
     });
-    
+    return res;
   };
+
+  public getUsersByUsername = (usernames: Array<string>) => {
+    const res: Array<IUser> = [];
+    usernames.forEach((username) => {
+      const targetUser = this.#users.find((u) => {
+        return username === u.username;
+      });
+      if (targetUser) res.push(targetUser);
+    });
+    return res;
+  };
+
+  public setUser = (user: IUser) => this.#users.push(user);
 }
