751152ef1d700532c6b60bf1aad795c19067a8c4
Eric Khosro <ericzed2002@gmail.com>
2023-04-16 15:34:45 +0330

    added login and get user functions to client class

--- Client/src/index.ts
+++ Client/src/index.ts
@@ -1,31 +1,20 @@
 import { Client } from "./client.js";
-import { GetUsersDTO, ILoginData } from "./interfaces";
 
 const client1 = new Client(1);
-
-// client1.connectToServer();
-// client1.sendToServer<ILoginData>({
-//   methodName: "login",
-//   body: {
-//     username: "user1",
-//     password: "123",
-//   },
-// });
+client1.connectToServer();
+client1.login("user1", "123");
 
 const client2 = new Client(2);
 
 client2.connectToServer();
-client2.sendToServer<ILoginData>({
-  methodName: "login",
-  body: {
-    username: "user2",
-    password: "321",
-  },
-});
+client2.login("user2", "321");
 
-client2.sendToServer<GetUsersDTO>({
-  methodName: "getUsers",
-  body: {
-    users: [{ id: 2 }],
-  },
-});
+client2.getUsers();
+
+
+
+
+
+
+
+
