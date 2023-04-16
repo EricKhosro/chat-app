7ead1668103f4f305ddccc374e1f3ecf14878eac
Eric Khosro <ericzed2002@gmail.com>
2023-04-16 15:38:08 +0330

    fixed server bug so ii dont create a class on every socket

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -3,10 +3,9 @@ import { ISocket } from "./interfaces/interfaces";
 import { TCPServer } from "./tcpServer.js";
 
 const server = new TCPServer();
+const socketHandler = new SocketHandler();
 server.setOnNewSocket((socket: ISocket) => {
-  const socketHandler = new SocketHandler();
   socketHandler.registerSocker(socket);
 });
 server.createServer();
 server.startListening(3000, "localhost");
-
