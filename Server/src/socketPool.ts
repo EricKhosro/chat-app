74216a6276c3c272b4549390d0068c4c97f8ad14
Eric Khosro <ericzed2002@gmail.com>
2023-04-16 15:39:29 +0330

    modified socket pool for null guids

--- Server/src/socketPool.ts
+++ Server/src/socketPool.ts
@@ -3,13 +3,15 @@ import { IRegisteredSocket } from "./interfaces/interfaces";
 export class SocketPool {
   #registeredSockets: Array<IRegisteredSocket> = [];
 
-  public getRegisteredSockets = (socketIds: Array<string>) => {
+  public getRegisteredSockets = (socketIds: Array<string | null>) => {
     const res: Array<IRegisteredSocket> = [];
     socketIds.forEach((sId) => {
-      const targetSocket = this.#registeredSockets.find((u) => {
-        return u.guid === sId;
-      });
-      if (targetSocket) res.push(targetSocket);
+      if (sId) {
+        const targetSocket = this.#registeredSockets.find((u) => {
+          return u.guid === sId;
+        });
+        if (targetSocket) res.push(targetSocket);
+      }
     });
     return res;
   };
