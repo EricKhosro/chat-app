d7b8d91f32d57503fb5a86ccde87ddc3b93de16b
Eric Khosro <ericzed2002@gmail.com>
2023-04-16 15:42:02 +0330

    moved get friends to user manager

--- Server/src/Methods/getUsers.ts
+++ Server/src/Methods/getUsers.ts
@@ -1,19 +1,10 @@
-import { Database } from "../db.js";
-import { GetUsersDTO, IGetUsersData } from "../interfaces/getUsersInterface";
-import { IFriends, IMethodClass } from "../interfaces/interfaces.js";
+import { UserManager } from "../userManager.js";
+import { IGetUsersRes } from "../interfaces/getUsersInterface";
+import { IMethodClass } from "../interfaces/interfaces.js";
 
-export class GetUsers
-  implements IMethodClass<GetUsersDTO | undefined, Array<IFriends>>
-{
-  public handle = (data: GetUsersDTO | undefined) => {
-    const db = new Database();
-    const res: Array<IFriends> = [];
-    db.getUsersByID().forEach((user) => {
-      data?.users.forEach((data) => {
-        if (data.id !== user.id)
-          res.push({ id: user.id, username: user.username });
-      });
-    });
-    return res;
+export class GetUsers implements IMethodClass<{}, IGetUsersRes> {
+  public handle = (): IGetUsersRes => {
+    const userManager = new UserManager();
+    return { methodname: "getUsers", users: userManager.getFriends() };
   };
 }
