73c413bdee4f5293d20b8eb29e9bf0d4ba84bbc1
Eric Khosrafian <erickhosrafian@gmail.com>
2023-05-11 15:37:54 +0330

    added is connected to context

--- Client/React/chat-app-react-client/src/App.tsx
+++ Client/React/chat-app-react-client/src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.css";
-import { IContext, ServerResponse } from "./Interfaces/commonDTOs";
+import { IContext, ServerResponse } from "./Interfaces/commonInterfaces";
 import Routes from "./Routes/Routes";
 import { useEffect, useState, createContext } from "react";
 
 export const Context = createContext<IContext>({
   socket: null,
   serverResponse: { methodName: "", body: {} },
+  isConnected: false,
 });
 
 function App() {
@@ -13,11 +14,14 @@ function App() {
   const [serverResponse, setServerResponse] = useState<ServerResponse>(
     {} as ServerResponse
   );
+  const [isConnected, setIsConnected] = useState(false);
+  console.log({ appServerResponse: serverResponse });
 
   useEffect(() => {
     const newSocket = new WebSocket("ws://localhost:5000");
     setSocket(newSocket);
     newSocket.onopen = () => {
+      setIsConnected(true);
       console.log("WebSocket connected");
     };
 
@@ -27,9 +31,10 @@ function App() {
 
     newSocket.onclose = () => {
       console.log("WebSocket connection closed");
-      setTimeout(() => {
-        setSocket(new WebSocket("ws://localhost:5000"));
-      }, 3000);
+      setIsConnected(false);
+      // setTimeout(() => {
+      //   setSocket(new WebSocket("ws://localhost:5000"));
+      // }, 3000);
     };
 
     return () => {
@@ -40,7 +45,7 @@ function App() {
 
   return (
     <div className="page">
-      <Context.Provider value={{ socket, serverResponse }}>
+      <Context.Provider value={{ socket, serverResponse, isConnected }}>
         <Routes />
       </Context.Provider>
     </div>
