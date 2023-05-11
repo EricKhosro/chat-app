664051788d62e76f979facf88abb30e398a55d6c
Eric Khosrafian <erickhosrafian@gmail.com>
2023-05-11 09:39:32 +0330

    modified get users

--- Server/src/Methods/getUsers.ts
+++ Server/src/Methods/getUsers.ts
@@ -3,10 +3,10 @@ import { IMethodClass } from "../interfaces/interfaces.js";
 import { globalUserManager } from "./login.js";
 
 export class GetUsers implements IMethodClass<{}, IGetUsersRes> {
-  public handle = (): IGetUsersRes => {
+  public handle = (data: {}, guid: string | null): IGetUsersRes => {
     return {
       methodName: "getUsers",
-      body: { users: globalUserManager.getFriends() },
+      body: { users: globalUserManager.getFriends(guid) },
     };
   };
 }
