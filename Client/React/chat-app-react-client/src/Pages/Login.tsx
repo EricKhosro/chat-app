e250c1192b8d08f4f06798e00b567a26ca3d4e89
Eric Khosrafian <erickhosrafian@gmail.com>
2023-05-11 09:36:58 +0330

    modified login page to navigate to messenger on login and toast on error

--- Client/React/chat-app-react-client/src/Pages/Login.tsx
+++ Client/React/chat-app-react-client/src/Pages/Login.tsx
@@ -1,20 +1,29 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import { LoginFormValues } from "../Interfaces/loginInterfaces";
 import TextInput from "../Components/TextInput";
 import Button from "../Components/Button";
 import { Context } from "../App";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const [values, setValues] = useState<LoginFormValues>({} as LoginFormValues);
 
-  const { socket } = useContext(Context);
+  const { socket, serverResponse } = useContext(Context);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!serverResponse || serverResponse.methodName !== "login") return;
+
+    if (!serverResponse.body.data) toast.error(serverResponse.body.msg);
+    if (serverResponse.body.data) navigate("/Messenger");
+  }, [serverResponse]);
 
   const changeHandler = (name: string, value: any) => {
     setValues({ ...values, [name]: value });
   };
 
   const clickHandler = () => {
-    console.log(socket);
     if (socket && socket.readyState === WebSocket.OPEN)
       socket?.send(
         JSON.stringify({
